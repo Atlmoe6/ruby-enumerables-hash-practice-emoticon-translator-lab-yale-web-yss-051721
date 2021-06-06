9ad5f0e99cf76c92e605a3ad42c646be0871c31c
abstract-inheritance-c040 <doe.mahamud1@gmail.com>
2021-06-06 17:50:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,10 +1,25 @@
-# require modules here
-
-def load_library
+require 'yaml'
+require 'pry'
+def load_library(file_path)
   # code goes here
+   emoticons = YAML.load_file(file_path)
+  emote_hash = {} 
+  
+  emoticons.each do|key, value|
+    emote_hash[key] = {} 
+    emote_hash[key][:english]= value [0]
+    emote_hash[key][:japanese]= value [1]
+    
+
+    
+end
+emote_hash
+
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file_path, emoticon)
+  
+ emote_library =  load_library(file_path)
   # code goes here
 end
 
