8305523720e4d819256ab088f50c155a3373e37c
abstract-inheritance-c040 <doe.mahamud1@gmail.com>
2021-06-06 18:01:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,7 +24,7 @@ def get_japanese_emoticon(file_path, emoticon)
    emote_library[key][:english] == emoticon
  end
 emoticon ? emote_library[emoticon][:japanese]
-end
+
 end
 
 def get_english_meaning(file_path, emoticon)
