d7a672660d122f9d088fa692c99896f3d5549ac4
abstract-inheritance-c040 <doe.mahamud1@gmail.com>
2021-06-06 17:59:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,9 +19,9 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   
-   emote_library = load_library(file_path)
-  emoticon = library.keys.find do |key|
-    emote_library[key][:english] == emoticon
+ emote_library =  load_library(file_path)
+ emoticon = library.keys.find do |key| 
+   emote_library[key][:english] == emoticon
  end
   # code goes here
 end
