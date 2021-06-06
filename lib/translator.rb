44266bd5477947357ec3bc1038e297f86d203482
abstract-inheritance-c040 <doe.mahamud1@gmail.com>
2021-06-06 18:13:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,17 +19,17 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   
- emote_library =  load_library(file_path)
- 
- emoticon = emote_library.keys.find do |key| 
-   emote_library[key][:english] == emoticon
-   
- end
-emoticon ? library[emoticon][:japanese] : "Sorry, that emoticon was not found"
-end
+  emote_library = load_library(file_path)
+  
+  emoticon = emote_library.keys.find do |key|
+    library[key][:english] == emoticon
+    
+  end
+  emoticon ? emote_library[emoticon][:japanese] : "Sorry, that emoticon was not found"
 
 def get_english_meaning(file_path, emoticon)
   # code goes here
+  
    emote_library =  load_library(file_path)
     emoticon = emote_library.keys.find do |key|
     emote_library[key][:japanese] == emoticon
